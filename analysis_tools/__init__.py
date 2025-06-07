ad233fe3798946a4e647e87246bf914c4b1aa91c
Laurence Paul Cook <lcook@lxplus961.cern.ch>
2025-06-07 02:40:13 +0200

    Add waveform processing functions

--- analysis_tools/__init__.py
+++ analysis_tools/__init__.py
@@ -1,3 +1,5 @@
 from .calibration_db_interface import CalibrationDBInterface
+from .waveform_processing import WaveformProcessing
 
 __all__ = ["CalibrationDBInterface"]
+__all__ = ["WaveformProcessing"]
