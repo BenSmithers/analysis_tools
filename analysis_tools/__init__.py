c73da16304bec4a75e5616a4142aed0e087e43d2
Laurence Paul Cook <lcook@lxplus959.cern.ch>
2025-06-21 01:59:06 +0200

    Add pulse finding functions

--- analysis_tools/__init__.py
+++ analysis_tools/__init__.py
@@ -1,5 +1,5 @@
 from .calibration_db_interface import CalibrationDBInterface
 from .waveform_processing import WaveformProcessing
+from .pulse_finding import do_pulse_finding, do_pulse_finding_vect
 
-__all__ = ["CalibrationDBInterface"]
-__all__ = ["WaveformProcessing"]
+__all__ = ["CalibrationDBInterface","WaveformProcessing","do_pulse_finding", "do_pulse_finding_vect"]
