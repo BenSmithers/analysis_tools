5b70eaec2ea7e6ac45cd35a6578b0b92a36133a9
Ben Smithers <bsmithers@triumf.ca>
2025-08-05 21:06:27 -0700

    added midas example

--- scripts/slow_control/examples/midas.py
+++ scripts/slow_control/examples/midas.py
@@ -1,11 +1,35 @@
-import numpy as np 
+
+import mysql.connector
+from tqdm import tqdm 
 
 
-def sample(thisdat, group=10):
-    """
-        Sample 1 of every GROUP in $thisdat
-    """
-    if group==1:
-        return thisdat 
-    mask = np.arange(len(thisdat)) % group == 0
-    return thisdat[mask]
+print("Opening MySQL connection to neutsrv2")
+mydb = mysql.connector.connect(
+    host="neutsrv2.triumf.ca",
+    user="history_reader",
+    password="reader_PsFtKL5kR3",
+    database="history"
+)
+
+print(mydb)
+cursor = mydb.cursor()
+
+
+print("Show all tables")
+tables = []
+cursor.execute("SHOW TABLES")
+for x in cursor:
+    
+    tables.append(str(x[0]))
+    print(tables[-1])
+
+cursor.execute("describe mpmtmon_e001")
+for x in cursor:
+    print(x)
+
+print("\nget data for 5 minutes on April 20, 2025")
+
+cursor.execute("select _i_time,m001_brb_press_temp,m001_brb_pressure from mpmtmon_e001 where _i_time > 1745208120 and _i_time < 1745208420")
+for x in cursor:
+    print(x)
+
